afaa02338a7a58ba3bfc10fd0ab5e74fefefb297
Neosoulink <neodarksoulink@gmail.com>
2022-02-16 17:43:25 +0100

    init lift-off-part 3

--- server/src/datasources/track-api.js
+++ server/src/datasources/track-api.js
@@ -1,18 +1,19 @@
-const { RESTDataSource } = require("apollo-datasource-rest");
+const { RESTDataSource } = require('apollo-datasource-rest');
 
 class TrackAPI extends RESTDataSource {
-	constructor() {
-		super();
-		this.baseURL = "https://odyssey-lift-off-rest-api.herokuapp.com/";
-	}
+  constructor() {
+    super();
+    // the Catstronauts catalog is hosted on this server
+    this.baseURL = 'https://odyssey-lift-off-rest-api.herokuapp.com/';
+  }
 
-	getTracksForHome() {
-		return this.get("tracks");
-	}
+  getTracksForHome() {
+    return this.get('tracks');
+  }
 
-	getAuthor(authorId) {
-		return this.get(`author/${authorId}`);
-	}
+  getAuthor(authorId) {
+    return this.get(`author/${authorId}`);
+  }
 }
 
 module.exports = TrackAPI;
