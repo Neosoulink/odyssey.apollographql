ccc7dc8af88807f627fca16e31cc9fc2ba225906
Neosoulink <neodarksoulink@gmail.com>
2022-02-16 14:13:01 +0100

    minor fix

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -3,12 +3,12 @@ const resolvers = {
 		// returns an array of Tracks that will be used to populate
 		// the homepage grid of our web client
 		tracksForHome: (_, __, { dataSources }) => {
-			return dataSources.tracksAPI.getTracksForHome();
+			return dataSources.trackAPI.getTracksForHome();
 		},
 	},
 	Track: {
 		author: ({ authorId }, _, { dataSources }) => {
-			return dataSources.tracksAPI.getAuthor(authorId);
+			return dataSources.trackAPI.getAuthor(authorId);
 		},
 	},
 };
