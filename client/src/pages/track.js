2bbe263babc90cae673d7e28c18a880c5e38cc8f
Neosoulink <neodarksoulink@gmail.com>
2022-02-17 20:24:07 +0100

    Building the track page

---
+++ client/src/pages/track.js
@@ -0,0 +1,9 @@
+import React from "react";
+import { gql, useQuery } from "@apollo/client";
+import { Layout, QueryResult } from "../components";
+
+const Track = ({ trackId }) => {
+	return <Layout></Layout>;
+};
+
+export default Track;
