2ea10c53a68361f7280c036f77181ff9b15199bd
Neosoulink <neodarksoulink@gmail.com>
2022-02-17 19:01:31 +0100

    Resolver args parameter

--- server/src/resolvers.js
+++ server/src/resolvers.js
@@ -1,15 +1,19 @@
 const resolvers = {
-  Query: {
-    // returns an array of Tracks that will be used to populate the homepage grid of our web client
-    tracksForHome: (_, __, { dataSources }) => {
-      return dataSources.trackAPI.getTracksForHome();
-    },
-  },
-  Track: {
-    author: ({ authorId }, _, { dataSources }) => {
-      return dataSources.trackAPI.getAuthor(authorId);
-    },
-  },
+	Query: {
+		// returns an array of Tracks that will be used to populate the homepage grid of our web client
+		tracksForHome: (_, __, { dataSources }) => {
+			return dataSources.trackAPI.getTracksForHome();
+		},
+		// get a single track by ID, for the track page
+		track: (_, { id }, { dataSources }) => {
+			return dataSources.trackAPI.getTrack(id);
+		},
+	},
+	Track: {
+		author: ({ authorId }, _, { dataSources }) => {
+			return dataSources.trackAPI.getAuthor(authorId);
+		},
+	},
 };
 
 module.exports = resolvers;
