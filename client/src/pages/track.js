6fbc5ae1cec92524833d12105bd74ba4ebc5c8c7
Neosoulink <neodarksoulink@gmail.com>
2022-02-17 21:01:00 +0100

    THE USEQUERY HOOK - WITH VARIABLES

--- client/src/pages/track.js
+++ client/src/pages/track.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { gql, useQuery } from "@apollo/client";
 import { Layout, QueryResult } from "../components";
+import TrackDetail from "../components/track-detail";
 
 export const GET_TRACK = gql`
 	query getTrack($trackId: ID!) {
@@ -31,7 +32,13 @@ const Track = ({ trackId }) => {
 		variables: { trackId },
 	});
 
-	return <Layout></Layout>;
+	return (
+		<Layout>
+			<QueryResult error={error} loading={loading} data={data}>
+				<TrackDetail track={data?.track} />
+			</QueryResult>
+		</Layout>
+	);
 };
 
 export default Track;
